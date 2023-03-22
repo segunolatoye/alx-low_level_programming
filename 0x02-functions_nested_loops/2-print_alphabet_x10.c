abd37f38b495cc8bcd23014e03d22be40357cfb9
segun <segunolatoye@gmail.com>
2023-03-22 22:21:28 +0000

    Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+/**
+ * print_alphabet_x10 - My Function
  * Description - Prints 10 times the alphabet, in lowercase.
  */
 
@@ -12,9 +13,9 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-			_putchar('\n');
-			i++;
 		}
+		_putchar('\n');
+		i++;
 	} while (i <= 10);
 }
 
